ca9952e8f7e5f71d42ec410341fd02e4834be784
Apexit <apexit@Apexits-MacBook-Air.local>
2024-09-15 21:38:44 +0530

    initial spring commit

---
+++ src/test/java/com/apexit/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
